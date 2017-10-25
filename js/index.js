250b3e2dba30a70e1c6286f953840117d9dd2abb
John Osterman <ostermanj@gmail.com>
2017-10-25 18:04:40 -0400

    Highlight on hover; zoom on click; Ill. Ind. and Mich. zoom out of bounds

--- js/index.js
+++ js/index.js
@@ -2,7 +2,9 @@
 
     "use strict";
     mapboxgl.accessToken = 'pk.eyJ1Ijoib3N0ZXJtYW5qIiwiYSI6ImNpdnU5dHVndjA2eDYyb3A3Nng1cGJ3ZXoifQ.Xo_k-kzGfYX_Yo_RDcHDBg';
-    const censusKey = '3a82a0bf1219fb2fb0962763bb1582c5951f8566';
+    const censusKey = '3a82a0bf1219fb2fb0962763bb1582c5951f8566',
+          originalCenter = [-96.2,38.28],
+          originalZoom = 3.9;
 
     const mapView = {
         
@@ -10,10 +12,16 @@
             this.map = new mapboxgl.Map({
                 container: 'map',
                 style: 'mapbox://styles/mapbox/light-v9',
-                zoom: 3.9,
-                center: [-96.2,38.28]
+                zoom: originalZoom,
+                center: originalCenter
                 
             });
+
+            // disable map rotation using right click + drag
+            this.map.dragRotate.disable();
+
+            // disable map rotation using touch rotation gesture
+            this.map.touchZoomRotate.disableRotation();
 
             window.map = this.map; // for access in console
 
@@ -82,6 +90,16 @@
 
             function addChloroLayer() {
 
+                function checkLayerLoaded(){
+                    if ( mapView.map.getLayer('states-join') && mapView.map.getLayer('states-join-hover') ){ // if addLayer below has taken effect
+                        console.log('render', mapView.map.getLayer('states-join'));
+                        mapView.map.off('render', checkLayerLoaded); //  turn off the listener for render
+                        mapView.setMouseEvents();
+                    }
+                }
+
+                this.map.on('render', checkLayerLoaded)
+
                 mapView.map.addLayer({
                     "id": "states-join",
                     "type": "fill",
@@ -95,6 +113,20 @@
                         }
                     }
                 }, 'water');
+
+                mapView.map.addLayer({
+                    "id": "states-join-hover",
+                    "type": "line",
+                    "source": "states",
+                    "source-layer": 'states',
+                    "paint": {
+                        "line-color": '#4D90FE',
+                        "line-width": 4,
+                        "line-blur": 2
+                    },
+                    "filter": ["==", "name", ""]
+                });
+
             }
 
             function addLegend() {
@@ -132,12 +164,69 @@
                 
             }
 
+        },
+        setMouseEvents(){
+
+            this.map.on('mousemove', 'states-join', e => {
+                if ( !controller.controlState.getState().isStateZoomed ){
+                    this.map.setFilter("states-join-hover", ["==", "STATEFP", e.features[0].properties.STATEFP]);  
+                }
+            });
+            this.map.on('mouseleave', 'states-join', e => {
+                this.map.setFilter("states-join-hover", ["==", "STATEFP", ""]);  
+            });
+            this.map.on('click', 'states-join', e => {
+                    console.log(controller.controlState.getState());
+                if ( !controller.controlState.getState().isStateZoomed ){
+                    controller.controlState.setState('isStateZoomed',true);
+                    var features = this.map.queryRenderedFeatures(e.point, {
+                        layers: ['states-join']
+                    });
+
+                    // some states have multiple coordinate arrays that need to be concatenated together
+                    // HT https://stackoverflow.com/a/10865042/5701184 for the [] concat apply method
+                    var coordinatesArray = features[0].geometry.coordinates.length === 1 ? [].concat.apply([], features[0].geometry.coordinates) : [].concat.apply([], [].concat.apply([], features[0].geometry.coordinates) );
+                    console.log(coordinatesArray);
+                        
+                    var minLon = d3.min(coordinatesArray, function(d){
+                        return d[0];
+                    });
+
+                    var minLat = d3.min(coordinatesArray, function(d){
+                        return d[1];
+                    });
+                    var maxLon = d3.max(coordinatesArray, function(d){
+                        return d[0];
+                    });
+                    var maxLat = d3.max(coordinatesArray, function(d){
+                        return d[1];
+                    });
+
+                    var bounds = [
+                        [minLon, minLat],
+                        [maxLon, maxLat]
+                    ];
+
+               //     console.log(JSON.stringify(bounds));
+                    map.fitBounds(bounds, {
+                        linear: true,
+                        padding: {top:10,right:150,bottom:10,left:10}
+                    });
+                } else {
+                    controller.controlState.setState('isStateZoomed',false);
+                    this.map.flyTo({
+                        center: originalCenter,
+                        zoom: originalZoom
+                    });
+                }
+            });
         }
     }; // end mapView
 
     const controller = {
-        
+        controlState: StateModule(),
         init(){
+        window.controller = controller;
             this.promises = {};
             this.getACSData('stateData','https://api.census.gov/data/2015/acs/acs5/profile?get=DP03_0099PE,NAME&for=state:*&key=')
             this.promises.stateData.then(values => {
@@ -170,6 +259,31 @@
             }, {}));
         }
     }; // end controller
+
+    
+    function StateModule() {
+        
+        var state = {};
+
+        function _logState(){
+            console.log(state);
+        }
+
+        function _getState(){
+            return state;
+        }
+
+        function _setState(key,value) { 
+                                   
+            state[key] = value;
+        }
+
+        return {
+            logState: _logState,
+            getState: _getState,
+            setState: _setState
+        }
+    }
 
     controller.init();   
 
